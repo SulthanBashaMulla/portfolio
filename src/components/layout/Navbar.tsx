b47ba00be7929de512cf43ef6856b32d8813a1f6
sultan-basha-NDC <mullasulthanbasha27@gmail.com>
2025-12-27 21:30:37 +0530

    update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -43,7 +43,7 @@ export const Navbar = () => {
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >
-            Portfolio
+            Mulla's Portfolio
           </motion.a>
 
           {/* Desktop Navigation */}
